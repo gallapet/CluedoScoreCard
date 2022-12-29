7c56d5d54ca0415a619ed03e45f1023c3b6c4264
Peter Gallacher <peter.gallacher@hotmail.co.uk>
2022-12-29 22:08:55 +0000

    auto cross for all rows

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 window.addEventListener("load", () => {
-    var cells = document.querySelectorAll(".player-cell, .result-cell");
-    cells.forEach((cell) => {
-        cell.innerHTML += "<select name=\"result\" class=\"result-select\"><option value=\"\"></option><option value=\"tick\">✅</option><option value=\"cross\">❌</option><option value=\"none\">O</option></select>";
-    })
+  var cells = document.querySelectorAll(".player-cell, .result-cell");
+  cells.forEach((cell) => {
+    cell.innerHTML +=
+      '<select name="result" class="result-select"><option value=""></option><option value="tick">✅</option><option value="cross">❌</option><option value="none">O</option></select>';
+  });
 
-    var row = document.querySelector("#green");
-    row.addEventListener("click", () => {
-        var playerCells = row.querySelectorAll("td.player-cell")
-        var resultCell = row.querySelector("td.result-cell").querySelector("select.result-select")
-        playerCells.forEach((cell) => {
-            console.log(cell.querySelector("select.result-select").value);
-            if (cell.querySelector("select.result-select").value === "cross") {
-                resultCell.value = "cross";
-            }
-        })
-    })
-
-})
+  var rows = document.querySelectorAll("tr.cell");
+  console.log(rows);
+  rows.forEach((row) => {
+    row.addEventListener("change", () => {
+      var playerCells = row.querySelectorAll("td.player-cell");
+      var resultCell = row
+        .querySelector("td.result-cell")
+        .querySelector("select.result-select");
+      playerCells.forEach((cell) => {
+        if (cell.querySelector("select.result-select").value === "cross") {
+          resultCell.value = "cross";
+        }
+      });
+    });
+  });
+});
