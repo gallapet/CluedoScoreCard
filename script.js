ab03363428e98f21586f6151703de88ec49447a0
Peter Gallacher <peter.gallacher@hotmail.co.uk>
2022-12-29 20:16:02 +0000

    add IDs to all rows, auto cross in result column

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 window.addEventListener("load", () => {
-    var playerCells = document.getElementsByName("player-cell");
-    playerCells.forEach((cell) => {
-        cell.innerHTML += "<select name=\"result\" id=\"result-select\"><option value=\"\"></option><option value=\"tick\">✅</option><option value=\"cross\">❌</option><option value=\"none\">O</option></select>"
+    var cells = document.querySelectorAll(".player-cell, .result-cell");
+    cells.forEach((cell) => {
+        cell.innerHTML += "<select name=\"result\" class=\"result-select\"><option value=\"\"></option><option value=\"tick\">✅</option><option value=\"cross\">❌</option><option value=\"none\">O</option></select>";
     })
+
+    var row = document.querySelector("#green");
+    row.addEventListener("click", () => {
+        var playerCells = row.querySelectorAll("td.player-cell")
+        var resultCell = row.querySelector("td.result-cell").querySelector("select.result-select")
+        playerCells.forEach((cell) => {
+            console.log(cell.querySelector("select.result-select").value);
+            if (cell.querySelector("select.result-select").value === "cross") {
+                resultCell.value = "cross";
+            }
+        })
+    })
+
 })
-
-
-
