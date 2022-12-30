6fbe4c5920a17ae35797e378f128f7557b4ac9ae
Peter Gallacher <peter.gallacher@hotmail.co.uk>
2022-12-30 10:49:32 +0000

    WIP: add tick when all columns are zero

--- script.js
+++ script.js
@@ -1,30 +1,80 @@
 window.addEventListener("load", () => {
-  var cells = document.querySelectorAll(".player-cell, .result-cell");
-  cells.forEach((cell) => {
+  var playerCellsAll = document.querySelectorAll("td.player-cell");
+  var resultCellsAll = document.querySelectorAll("td.result-cell");
+
+  playerCellsAll.forEach((cell) => {
     cell.innerHTML +=
-      '<select class="result"><option value=""></option><option value="tick">✅</option><option value="cross">❌</option><option value="none">O</option></select>';
+      '<select class="result"><option value=""><option value="cross">❌</option><option value="zero">O</option></option><option value="tick">✅</option></select>';
+  });
+  resultCellsAll.forEach((cell) => {
+    cell.innerHTML +=
+      '<select class="result"><option value=""><option value="cross">❌</option><option value="zero">O</option></option><option value="tick">✅</option><option value="clear">Clear</option></select>';
   });
 
   var rows = document.querySelectorAll("tr.cell");
+
   rows.forEach((row) => {
-    var playerCells = row.querySelectorAll("td.player-cell");
-    var resultCellOption = row
-      .querySelector("td.result-cell")
-      .querySelector("select.result");
+    var playerCellsRow = row.querySelectorAll("td.player-cell");
+    var resultCellRow = row.querySelector("td.result-cell");
+    var resultCellOption = resultCellRow.querySelector("select.result");
+
     row.addEventListener("change", () => {
-      console.log(playerCells);
-      playerCells.forEach((cell) => {
+      playerCellsRow.forEach((cell) => {
         if (cell.querySelector("select.result").value === "cross") {
           resultCellOption.value = "cross";
+          setRestOfRowAsZero(playerCellsRow);
         }
+        console.log(cell.children[0].value);
       });
-      if (resultCellOption.value === "cross") {
-        playerCells.forEach((cell) => {
-          if (cell.querySelector("select.result").value === "") {
-            cell.querySelector("select.result").value = "none";
-          }
+      console.log(cellsAreAllZero(playerCellsRow));
+      console.log(cellsAreAllEmpty(playerCellsRow));
+      // if (cellsAreAllZero(playerCellsRow)) {
+      //   resultCellOption.value = "tick";
+      // }
+    });
+
+    resultCellRow.addEventListener("change", () => {
+      if (resultCellOption.value === "clear") {
+        playerCellsRow.forEach((cell) => {
+          cell.querySelector("select.result").value = "";
+          resultCellOption.value = "";
+        });
+      } else if (
+        resultCellOption.value === "cross" &&
+        !cellsAreAllEmpty(playerCellsRow)
+      ) {
+        playerCellsRow.forEach((cell) => {
+          cell.querySelector("select.result").value = "zero";
         });
       }
     });
   });
 });
+
+function cellsAreAllEmpty(nodeList) {
+  console.log(nodeList);
+  nodeList.forEach((el) => {
+    if (el.children[0].value !== "") {
+      return false;
+    }
+  });
+  return true;
+}
+
+function cellsAreAllZero(nodeList) {
+  nodeList.forEach((el) => {
+    if (
+      el.children[0].value !== "zero") {
+      return false;
+    }
+  });
+  return true;
+}
+
+function setRestOfRowAsZero(nodeList) {
+  nodeList.forEach((el) => {
+    if (el.querySelector("select.result").value !== "cross") {
+      el.querySelector("select.result").value = "zero";
+    }
+  });
+}
